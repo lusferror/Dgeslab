96831d88a27fe07f8443efe689ed6b26caba6d40
lusferror <luisalfredorobles09@gmail.com>
2022-11-28 21:54:53 -0300

    se agrega hola mundo

--- src/js/page/home.js
+++ src/js/page/home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = ()=>{
     return (
         <div>
-            <h1>Bienvenidos a Dgeslab</h1>
+            <h1>Bienvenidos a Hola mundo</h1>
         </div>
     )
 }
