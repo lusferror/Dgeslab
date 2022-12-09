e22c032921e09cadabef5c440cadc8cf3efb3122
lusferror <luisalfredorobles09@gmail.com>
2022-12-09 19:22:42 -0300

    se configura aspectos visuales

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ScatterChart from "./graphs.jsx";
 
-export const Navbar = () => {
+export const Navbar = ({contenido}) => {
     return (
         <div className="">
-            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark w-100">
                 {/* <!-- Navbar Brand--> */}
-                <a className="navbar-brand ps-3 fs-3 " href="/">Dgeslab</a>
-                {/* <!-- Sidebar Toggle--> */}
-                <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
-                {/* <!-- Navbar Search--> */}
-                <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                    {/* <div className="input-group">
-                        <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                        <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
-                    </div> */}
-                </form>
+                <div className="col pt-2">
+                    <a className="navbar-brand ps-3 fs-3 " href="/">Dgeslab</a>
+                    <button className="btn btn-link btn-sm order-1 order-lg-0 m-4 me-lg-0 mb-3" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
+                </div>
+                {/* <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                
+                </form> */}
                 {/* <!-- Navbar--> */}
+                <div className="col"></div>
                 <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
@@ -42,13 +41,13 @@ export const Navbar = () => {
                                 <div className="sb-sidenav-menu-heading text-warning fst-italic">Operaciones</div>
                                 <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="true" aria-controls="collapseLayouts">
                                     <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
-                                        Produccion
+                                    Produccion
                                     <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
                                 </a>
                                 <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" >
                                     <nav className="sb-sidenav-menu-nested nav">
-                                        <a className="nav-link" href="/">Aprobar Equipos</a>
-                                        <a className="nav-link" href="#">Móviles</a>
+                                        <Link to='/Verificacion' className="nav-link" href="/">Aprobar Equipos</Link>
+                                        <Link to='/Procesamiento' className="nav-link" >Móviles</Link>
                                     </nav>
                                 </div>
                                 <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
@@ -101,8 +100,8 @@ export const Navbar = () => {
                 </div>
                 <div id="layoutSidenav_content">
                     <main className="p-3">
-                        
-                        <ScatterChart/>
+
+                        {contenido}
                     </main>
                     <footer className="py-4 bg-light mt-auto">
                         <div className="container-fluid px-4">
--- src/layout.js
+++ src/layout.js
@@ -15,129 +15,16 @@ const Layout = () => {
 
     return (
         <BrowserRouter >
-            <div>
-                <div className="sb-nav-fixed">
-                    {/* en el visual code minimicen el nav ----------------------------------------------------------------------------------------------*/}
-                    <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-                        <a className="navbar-brand ps-3 fs-3 " href="/">Dgeslab</a>
-                        <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
-                        <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                        </form>
-                        <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
-                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#!">Perfil</a></li>
-                                    <li><a className="dropdown-item" href="#!">Ajustes</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#!">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div id="layoutSidenav">
-                        {/* minicen  layoutSidenav_nav*-------------------------------------------------------------------------------------------------------*/}
-                        <div id="layoutSidenav_nav">
-                            <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                                <div className="sb-sidenav-menu contenedor">
-                                    <div className="nav">
-                                        <div className="sb-sidenav-menu-heading text-warning fst-italic">Inicio</div>
-                                        <a className="nav-link" href="index.html">
-                                            <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
-                                            Dashboard
-                                        </a>
-                                        <div className="sb-sidenav-menu-heading text-warning fst-italic">Operaciones</div>
-                                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="true" aria-controls="collapseLayouts">
-                                            <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
-                                            Produccion
-                                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                                        </a>
-                                        <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" >
-                                            <nav className="sb-sidenav-menu-nested nav">
-                                                <Link to='Verificacion' className="nav-link">
-                                                    Aprobar Equipos
-                                                    </Link> 
-                                                <Link to='/Procesamiento' className="nav-link">Revisión Móviles</Link>
-                                            </nav>
-                                        </div>
-                                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                                            <div className="sb-nav-link-icon"><i className="fas fa-book-open"></i></div>
-                                            Supervisor
-                                            <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                                        </a>
-                                        <div className="collapse" id="collapsePages" aria-labelledby="headingTwo" >
-                                            <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="true" aria-controls="pagesCollapseAuth">
-                                                    Usuarios
-                                                    <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                                                </a>
-                                                <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                                    <nav className="sb-sidenav-menu-nested nav">
-                                                        <a className="nav-link" href="login.html">Registro</a>
-                                                        <a className="nav-link" href="register.html">Tabla de Usuarios</a>
-                                                        <a className="nav-link" href="password.html">Restablecer Contraseña</a>
-                                                    </nav>
-                                                </div>
-                                                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                                    Bodega
-                                                    <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                                                </a>
-                                                <div className="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                                    <nav className="sb-sidenav-menu-nested nav">
-                                                        <a className="nav-link" href="401.html">Recepcion</a>
-                                                        <a className="nav-link" href="404.html">Despacho</a>
-                                                        <a className="nav-link" href="500.html">Bodega</a>
-                                                    </nav>
-                                                </div>
-                                            </nav>
-                                        </div>
-                                        <div className="sb-sidenav-menu-heading text-warning fst-italic">Utilidad</div>
-                                        <a className="nav-link" href="#">
-                                            <div className="sb-nav-link-icon"><i className="fas fa-chart-area"></i></div>
-                                            Rendimiento
-                                        </a>
-                                        <a className="nav-link" href="#">
-                                            <div className="sb-nav-link-icon"><i className="fas fa-table"></i></div>
-                                            Reportes
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="sb-sidenav-footer">
-                                    <div className="small">Usuario:</div>
-                                    Supervisor
-                                </div>
-                            </nav>
-                        </div>
-                        <div id="layoutSidenav_content">
-                            <main className="p-3">
+            <Navbar contenido={
+                <Routes>
+                    <Route element={<Home />} path="/" />
+                    <Route element={<HomeTecnico />} path="/HomeTecnico" />
+                    <Route element={<Verificacion />} path="/Verificacion" />
+                    <Route element={<Procesamiento />} path="/Procesamiento" />
+                    <Route element={<HomeSupervisor />} path="/HomeSupervisor" />
 
-                                <Routes>
-                                    <Route element={<Home />} path="/" />
-                                    <Route element={<HomeTecnico />} path="/HomeTecnico" />
-                                    <Route element={<Verificacion />} path="/Verificacion" />
-                                    <Route element={<Procesamiento />} path="/Procesamiento" />
-                                    <Route element={<HomeSupervisor />} path="/HomeSupervisor" />
-
-                                </Routes>
-
-                            </main>
-                            {/* minimicen el footer */}
-                            <footer className="py-4 bg-light mt-auto">
-                                <div className="container-fluid px-4">
-                                    <div className="d-flex align-items-center justify-content-between small">
-                                        <div className="text-muted">Copyright &copy; Your Website 2022</div>
-                                        <div>
-                                            <a href="#">Privacy Policy</a>
-                                            &middot;
-                                            <a href="#">Terms &amp; Conditions</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </footer>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                </Routes>}>
+            </Navbar>
         </BrowserRouter>
     );
 };
