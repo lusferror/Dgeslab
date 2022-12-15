5ae0c6b6a43d7356d4abe83528a5d4d39743658d
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-12-15 14:10:23 -0300

    Agregado/embalaje/empacado

--- src/layout.js
+++ src/layout.js
@@ -14,6 +14,8 @@ import Recupera from "./js/page/recupera"
 import Registrar from "./js/page/registrar"
 import Recepcion from "./js/page/recepcion"
 import Asignar from "./js/page/asignar"
+import Empacado from "./js/page/Empacado";
+import Embalaje from "./js/page/Embalaje";
 // ---------------------------------- contexto ------------------------------------
 import { Context } from "../src/js/store/appContext";
 
@@ -35,6 +37,8 @@ const Layout = () => {
                     <Route element={<Registrar />} path="/registrar" />
                     <Route element={<Recepcion />} path="/recepcion" />
                     <Route element={<Asignar/>} path="/asignar" />
+                    <Route element={<Empacado/>} path="/Empacado" />
+                    <Route element={<Embalaje/>} path="/Embalaje" />
                 </Routes>
             }>
             </Navbar>
